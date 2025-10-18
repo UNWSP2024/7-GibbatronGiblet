f32d960381816ec9b55627ad4ea2692757e5518d
GibbatronGiblet <lbgibson@students.unwsp.edu>
2025-10-17 19:25:42 -0500

    Update program_3.py

--- program_3.py
+++ program_3.py
@@ -1,29 +1,29 @@
 # Program #3: US_Population
 def main():
     value_count = 3
-    master_list = []
+    all_entered_values = []
+    rep_number = 1
     for population_list in range(value_count):
         population_list = []
-        rep_number = 1
         year = int(input(f"Enter year {rep_number}'s number: "))
         state = input(f"Enter state {rep_number}'s name: ")
         population = int(input(f"Enter population {rep_number}'s value: "))
         population_list.append(year)
         population_list.append(state)
         population_list.append(population)
-        master_list.append(population_list)
+        all_entered_values.append(population_list)
         rep_number += 1
 
-    print("These are the values you have entered:",master_list)
-    sum_year = int(input("What year do you want the total population for?"))
-    print(f"This year's population is {sum_population_for_year(master_list, sum_year)}.")
+    print("These are the values you have entered:",all_entered_values)
+    year_to_sum = int(input("What year do you want the total population for?"))
+    print(f"{year_to_sum}'s population is {sum_population_for_year(all_entered_values, year_to_sum)}.")
 
 
-def sum_population_for_year(master_list, sum_year):
-    meta_list = list(master_list)
+def sum_population_for_year(all_entered_values, year_to_sum):
+    meta_list = list(all_entered_values)
     total_population = 0
     for item in meta_list:
-        if item[0] == sum_year:
+        if item[0] == year_to_sum:
             total_population += item[2]
     return total_population
 
